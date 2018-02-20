851a451792cd892ab566c3a56e917dc1d0971531
Jacob Biros <jake@Jacobs-MacBook-Pro.local>
2018-02-20 23:06:11 +0900

    Change the routes being called by axios to api routes

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/daily', weather_controller.daily)
+app.get('/api/now', weather_controller.now)
+app.get('/api/daily', weather_controller.daily)
 
 app.listen(3000, function () {
     console.log('Listening on port 3000...')
