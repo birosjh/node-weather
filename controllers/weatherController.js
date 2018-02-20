d376c1d575f3b24528cbaec183802495e19cf610
Jacob Biros <jake@Jacobs-MacBook-Pro.local>
2018-02-20 23:05:37 +0900

    Make getWeather function a wrapper for axios calls to openWeatherMap

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -1,19 +1,29 @@
 const axios = require('axios')
+require('dotenv').config()
+
+exports.now = async function (req, res) {
+    var parameters = {
+        q: 'London,uk',
+        appid: process.env.WEATHER_API
+    }
+
+    var weather = await getWeather(parameters);
+    console.log(weather)
+    res.send(weather);
+};
 
 exports.daily = function(req, res) {
     
-    getDailyWeather();
+    getWeather();
     res.send('Hello World!')
 };
 
-async function getDailyWeather() {
+async function getWeather(parameters) {
     try {
-        let response = await axios.get('https://www.metaweather.com/api/location/search/', {
-            params: {
-                'query': 'london'
-            }
+        let response = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+            params: parameters
         });
-        console.log(response.data)
+        return response.data;
     } catch (err) {
         console.log(err)
     }
